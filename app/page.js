67fd60d2c78f523e757b7d72319cc22e6bb87df5
mrcver <wataly.sh@gmail.com>
2024-11-09 23:04:38 +0800

    complete

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
-"use client" ;
+"use client";
 import styles from "./page.module.css";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 import Link from "next/link";
 import ficofiLogo from "/public/FICOFI_Community.svg"
@@ -14,8 +14,10 @@ export default function Home() {
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        "https://ficofi-community-backend.azurewebsites.net/proofs",
-        { headers: {"Authorization" : `Basic Zmljb2ZpOjNibWVpYTc0TzFOdA==`} }
+        // TODO replace prd url
+        // "https://ficofi-community-backend.azurewebsites.net/register-list",
+        "https://ficofi-community-backend-dev-f3c9eabeb5akcsb4.southeastasia-01.azurewebsites.net/register-list",
+        {headers: {"Authorization": `Basic Zmljb2ZpOjNibWVpYTc0TzFOdA==`}}
       );
       setLoading(false)
       setData(result.data);
@@ -26,19 +28,19 @@ export default function Home() {
     return (
       <main className={styles.main}>
         <header className={styles.header}>
-          <Image 
+          <Image
             src={ficofiLogo}
             width={225}
             height={75}
-            alt="FICOFI Community" />
-        <h1 className={styles.subtitle}>Proofs of consumption</h1>
+            alt="FICOFI Community"/>
+          <h1 className={styles.subtitle}>Event register</h1>
         </header>
-        <Image 
-              src={loader}
-              width={100}
-              height={100}
-              className={styles.loader}
-              alt="FICOFI Community" />
+        <Image
+          src={loader}
+          width={100}
+          height={100}
+          className={styles.loader}
+          alt="FICOFI Community"/>
       </main>
     )
   }
@@ -46,46 +48,40 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <header className={styles.header}>
-        <Image 
-              src={ficofiLogo}
-              width={225}
-              height={75}
-              alt="FICOFI Community" />
-        <h1 className={styles.subtitle}>Proofs of consumption</h1>
+        <Image
+          src={ficofiLogo}
+          width={225}
+          height={75}
+          alt="FICOFI Community"/>
+        <h1 className={styles.subtitle}>Event register</h1>
       </header>
       <table className={styles.table}>
         <thead className={styles.thead}>
-          <tr>
-            <th>Order</th>
-            <th>Item</th>
-            <th className={styles.centerAlign}>Date</th>
-            <th>Customer</th>
-            <th>Community</th>
-            <th>Photos</th>
-          </tr>
+        <tr>
+          <th>Event</th>
+          <th>Customer Name</th>
+          <th>Customer Dietary Restrictions</th>
+          <th>Guest Name</th>
+          <th>Guest Dietary Restrictions</th>
+          <th>Additional Notes</th>
+          <th className={styles.centerAlign}>Date</th>
+        </tr>
         </thead>
         <tbody>
-          {data.map((item) => (
-            <tr key={item.proof_id} className={styles.tr}>
-              <td><pre>{item.order_id}</pre></td>
-              <td><p><pre>{item.item_id}</pre></p><p>{item.name_en} <strong>{item.vintage}</strong></p></td>
-              <td className={styles.centerAlign}><p>{new Date(item.timestamp).toLocaleString().split(" ")[0]}</p><p>{new Date(item.timestamp).toLocaleString().split(" ")[1]}</p></td>
-              <td><p><strong>{item.customer_id}</strong></p><p>{item.name}</p></td>
-              <td><p><strong>{item.community_code}</strong></p><p>{item.community_name}</p></td>
-              <td>{
-                item.filenames.split(";").map(element => {
-                  return  <Link href={`https://ficoficlientsazurec96aa.blob.core.windows.net/ficofi-proofs-of-consumption/${item.proof_id}/${element}`} key="link">
-                    <img
-                      src={`https://ficoficlientsazurec96aa.blob.core.windows.net/ficofi-proofs-of-consumption/${item.proof_id}/${element}`}
-                      width={80}
-                      height={100}
-                      alt="Proof of consumption"
-                    />
-                  </Link>
-                })
-            }</td>
-            </tr>
-          ))}
+        {data.map((item) => (
+          <tr key={item.register_id} className={styles.tr}>
+            <td>
+              <pre>{item.event_name}</pre>
+            </td>
+            <td><p>{item.customer_name}</p></td>
+            <td><p>{item.customer_dietary_restrictions}</p></td>
+            <td><p>{item.guest_name}</p></td>
+            <td><p>{item.guest_dietary_restrictions}</p></td>
+            <td><p>{item.notes}</p></td>
+            <td className={styles.centerAlign}><p>{new Date(item.timestamp).toLocaleString().split(" ")[0]}</p>
+              <p>{new Date(item.timestamp).toLocaleString().split(" ")[1]}</p></td>
+          </tr>
+        ))}
         </tbody>
       </table>
     </main>
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/ficofi-community-register',
+  basePath: '/ficofi-event-register',
   output: 'export',
   images: {
     domains: ['ficoficlientsazurec96aa.blob.core.windows.net'],
